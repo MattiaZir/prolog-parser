345adbea519abbd470ba34c633a2a0503a9bf57b
Mattia Zirpoli <mat.zirpoli@gmail.com>
2021-12-11 00:36:43 +0100

    initial commit

---
+++ uriparser.pl
@@ -0,0 +1,61 @@
+% URI %
+uri --> scheme, [:], authority, [/], path, [?], query, [#], fragment.
+uri --> scheme, [:], authority, [/], path, [?], query.
+uri --> scheme, [:], authority, [/], path, [#], fragment.
+uri --> scheme, [:], authority, [/], path.
+uri --> scheme, [:], authority, [/].
+
+scheme --> identifier.
+
+% AUTHORITY %
+authority --> double_slash, userinfo, [@], host, [:], port.
+authority --> double_slash, host, [:], port.
+authority --> double_slash, userinfo, [@], host.
+authority --> double_slash, host.
+
+host --> host_identifier.
+host --> host_identifier, [.], host.
+
+userinfo --> identifier.
+
+% PATH %
+path --> identifier, [/], path.
+path --> identifier.
+
+% QUERY %
+query --> [Q], { Q \= '#' }.
+query --> query, [Q], {Q \= '#'}.
+
+% FRAGMENT %
+fragment --> [F], { char_type(F, ascii) }.
+fragment --> fragment, [F], { char_type(F, ascii) }. 
+
+% IDENTIFIERS %
+identifier --> [I], { valid_id_char(I) }.
+identifier --> [I], { valid_id_char(I) }, identifier.
+
+host_identifier --> [H], { valid_host_char(H) }.
+host_identifier --> [H], { valid_host_char(H) }, host_identifier.
+
+port --> [P], { char_type(P, digit) }.
+port --> [P], { char_type(P, digit) }, port.
+
+
+double_slash --> [/], [/].
+
+
+% VARIE %
+uri_parse(URIString, URI) :-
+        atom_chars(URIString, List),
+        phrase(uri, List).
+
+valid_id_char(Char) :-
+        Char \= '/',
+        Char \= '?',
+        Char \= '#',
+        Char \= '@',
+        Char \= ':'.
+
+valid_host_char(Char) :-
+        valid_id_char(Char),
+        Char \= '.'.
