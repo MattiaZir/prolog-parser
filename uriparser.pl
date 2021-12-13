a0148a35033c1ecaa53e0ecf762de94e07e3f802
Mattia Zirpoli <mat.zirpoli@gmail.com>
2021-12-13 23:49:44 +0100

    Refactoring

--- uriparser.pl
+++ uriparser.pl
@@ -1,55 +1,93 @@
 % URI %
-uri --> scheme, [:], authority, [/], path, [?], query, [#], fragment.
-uri --> scheme, [:], authority, [/], path, [?], query.
-uri --> scheme, [:], authority, [/], path, [#], fragment.
-uri --> scheme, [:], authority, [/], path.
-uri --> scheme, [:], authority, [/].
+uri(Scheme, Userinfo, Host, Port, Path, Query, Fragment) -->
+        scheme(Scheme),
+        authority(Userinfo, Host, Port),
+        after_authority(Path, Query, Fragment).
 
-scheme --> identifier.
+
+% URI 3 %
+/*
+uri --> scheme, [:], scheme_syntax.
+*/
+
+
+scheme(Scheme) --> identifier(Scheme), [:].
 
 % AUTHORITY %
-authority --> double_slash, userinfo, [@], host, [:], port.
-authority --> double_slash, host, [:], port.
-authority --> double_slash, userinfo, [@], host.
-authority --> double_slash, host.
+authority(Userinfo, Host, Port) --> double_slash, userinfo(Userinfo),
+        host(Host), port(Port).
+authority([]) --> [].
 
-host --> host_identifier.
-host --> ip_address, !.
-host --> host_identifier, [.], host.
+host(Host) --> host_identifier(HA), [.], host(HB), !, 
+        { append([HA, ['.'], HB], Host) }.
+host(Host) --> host_identifier(Host), !.
+host(Host) --> ip(Host).
 
-userinfo --> identifier.
+userinfo(Userinfo) --> identifier(Userinfo), [@].
+userinfo([]) --> [].
 
 % PATH %
-path --> identifier, [/], path.
-path --> identifier.
+
+after_authority(Path, Query, Fragment) --> [/], path(Path), query(Query), fragment(Fragment).
+after_authority([], [], []) --> [].
+path(Path) --> identifier(PA), [/], path(PB), !,
+        { append([PA, ['/'], PB], Path)}.
+path(Path) --> identifier(Path).
+path([]) --> [].
 
 % QUERY %
-query --> [Q], { Q \= '#' }.
-query --> [Q], {Q \= '#'}, query.
+query(Query) --> [?], valid_query(Query).
+query([]) --> [].
+
+valid_query(Query) --> [Q_head], { Q_head \= '#' }, 
+        valid_query(Q_rest), { Query = [Q_head | Q_rest] }.
+valid_query(Query) --> [Q_head], { Q_head \= '#', Query = [Q_head] }.
+
 
 % FRAGMENT %
-fragment --> [F], { char_type(F, ascii) }.
-fragment --> [F], { char_type(F, ascii) }, fragment. 
+fragment(Fragment) --> [#], valid_fragment(Fragment).
+fragment([]) --> [].
+
+valid_fragment(Fragment) -->  [F_head], 
+        valid_fragment(F_rest), { Fragment = [F_head | F_rest] }.
+valid_fragment(Fragment) --> [F_head], { Fragment = [F_head] }.
 
 % IDENTIFIERS %
-identifier --> [I], { valid_id_char(I) }.
-identifier --> [I], { valid_id_char(I) }, identifier.
+identifier(I) --> [I_head], { valid_id_char(I_head) }, 
+                identifier(I_rest), !, { I = [I_head | I_rest] }.
+identifier(I) --> [I_head], { valid_id_char(I_head), I = [I_head] }.
 
-host_identifier --> [H], { valid_host_char(H) }.
-host_identifier --> [H], { valid_host_char(H) }, host_identifier.
+% HOST IDENTIFIER %
+host_identifier(H) --> [H_head], { valid_host_char(H_head) }, 
+        host_identifier(H_rest), !, { H = [H_head | H_rest] }.
+host_identifier(H) --> [H_head], { valid_host_char(H_head), H = [H_head] }.
 
-% FAI IP %
+% IP %
+ip(Ip) --> ip_number(D1),
+        [.], ip_number(D2),
+        [.], ip_number(D3),
+        [.], ip_number(D4),
+        { append([D1, [.], D2, [.], D3, [.], D4], Ip) }.
+ip([]) --> [].
 
-port --> [P], { char_type(P, digit) }.
-port --> [P], { char_type(P, digit) }, port.
-port --> [80].
+ip_number(Dig) --> [D1, D2, D3], 
+        { valid_ip_number(D1, D2, D3), Dig = [D1, D2, D3] }.
 
+% PORT %
+port(Port) --> [:], !, port_valid(Port).
+port(['8', '0']) --> [], !.
+
+port_valid(Port) --> [P_head], { char_type(P_head, digit) },
+        port_valid(P_rest), !, { Port = [P_head | P_rest] }.
+port_valid(Port) --> [P_head], { char_type(P_head, digit), Port = [P_head] }, !.
+
+% VARIE %
 double_slash --> [/], [/].
 
 % VARIE %
-uri_parse(URIString, URI) :-
-        atom_chars(URIString, List),
-        phrase(uri, List).
+uri_parse(URIString, uri(Sch, UI, Ho, Po, Pa, Qu, Fr)) :-
+        string_chars(URIString, List),
+        phrase(uri(Sch, UI, Ho, Po, Pa, Qu, Fr), List).
 
 valid_id_char(Char) :-
         Char \= '/',
@@ -62,7 +100,24 @@ valid_host_char(Char) :-
         valid_id_char(Char),
         Char \= '.'.
 
-valid_ip_number(Char) :-
-        char_type(Char, digit),
-        Char =< 255,
-        Char >= 0.
+valid_ip_number(D1, D2, D3) :-
+        char_type(D1, digit),
+        char_type(D2, digit),
+        char_type(D3, digit),
+        number_string(Number, [D1, D2, D3]),
+        Number >= 0,
+        Number =< 255.
+
+% TEST %
+test_uri_1_success :-
+        uri_parse("http://www.google.com/we/dir?query#fragment", _).
+test_uri_2_success :-
+        uri_parse("http://www.google.com/we/dir?query", _).
+test_uri_3_success :-
+        uri_parse("http://www.google.com/we/dir", _).
+test_uri_4_success :-
+        uri_parse("http://www.google.com/", _).
+test_uri_5_success :-
+        uri_parse("http://www.google.com/we/", _).
+test_uri_6_success :-
+        uri_parse("http:", _).
