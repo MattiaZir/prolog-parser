a221dde735ea056ff125d2c12d7b83376fc759d0
Mattia Zirpoli <mat.zirpoli@gmail.com>
2021-12-15 15:13:57 +0100

    Completo - Sistemare path e testare.

--- uriparser.pl
+++ uriparser.pl
@@ -50,25 +50,29 @@ after_authority_zos(Path, Query, Fragment) --> [/], path_zos(Path),
 after_authority_zos([], [], []) --> [].
 
 % HOST %
-host(Host) --> host_identifier(HA), [.], host(HB),
-        { append([HA, ['.'], HB], Host) }.
-host(Host) --> host_identifier(Host).
+host(Host) --> host_identifier(HA_ch), [.], host(HB),
+        { atom_chars(HA, HA_ch), atomic_list_concat([HA, '.', HB], Host) }.
+host(Host) --> host_identifier(Host_ch), { atom_chars(Host, Host_ch)}.
 host(Host) --> ip(Host).
 
 % USER INFO %
-userinfo(Userinfo) --> identifier(Userinfo).
+userinfo(Userinfo) --> identifier(Userinfo_ch), 
+        { atom_chars(Userinfo, Userinfo_ch)}.
 
 % PATH %
-path(Path) --> identifier(PA), [/], path(PB),
-        { append([PA, ['/'], PB], Path)}.
-path(Path) --> identifier(Path).
+path(Path) --> valid_path(Path_ch), { atom_chars(Path, Path_ch) }.
 path([]) --> [].
 
-path_zos(Path) --> id44(PZA), { length(PZA, L), L =< 44 },
+valid_path(Path) --> identifier(PA), [/], valid_path(PB), 
+        { append([PA, ['/'], PB], Path) }.
+valid_path(Path) --> identifier(Path).
+
+path_zos(Path) --> valid_path_zos(Path_ch), { atom_chars(Path, Path_ch) }.
+
+valid_path_zos(Path) --> id44(PZA), { length(PZA, L), L =< 44 },
         ['('], id8(PZB), { length(PZB, L2), L2 =< 8}, [')'],
         { append([PZA, ['('], PZB, [')']], Path) }.
-path_zos(Path) --> id44(Path).
-path_zos([]) --> [].
+valid_path_zos(Path) --> id44(Path).
 
 % ID44 %
 id44(Id) --> valid_id44(IdA), [.], id44(IdB), {append([IdA, ['.'], IdB], Id)}.
@@ -94,7 +98,7 @@ valid_id8(Id) --> [Id_head], valid_id44(Id_rest),
 valid_id8(Id) --> [Id_head], { Id = [Id_head] }.
 
 % QUERY %
-query(Query) --> [?], valid_query(Query).
+query(Query) --> [?], valid_query(Query_ch), { atom_chars(Query, Query_ch)}.
 query([]) --> [].
 
 valid_query(Query) --> [Q_head], { Q_head \= '#' }, 
@@ -102,7 +106,8 @@ valid_query(Query) --> [Q_head], { Q_head \= '#' },
 valid_query(Query) --> [Q_head], { Q_head \= '#', Query = [Q_head] }.
 
 % FRAGMENT %
-fragment(Fragment) --> [#], valid_fragment(Fragment).
+fragment(Fragment) --> [#], valid_fragment(Fragment_ch), 
+        { atom_chars(Fragment, Fragment_ch)}.
 fragment([]) --> [].
 
 valid_fragment(Fragment) -->  [F_head], 
@@ -120,18 +125,24 @@ host_identifier(H) --> [H_head], { valid_host_char(H_head) },
 host_identifier(H) --> [H_head], { valid_host_char(H_head), H = [H_head] }.
 
 % IP %
-ip(Ip) --> ip_number(D1),
-        [.], ip_number(D2),
-        [.], ip_number(D3),
-        [.], ip_number(D4),
-        { append([D1, [.], D2, [.], D3, [.], D4], Ip) }.
+ip(Ip) --> ip_number(D1_ch),
+        [.], ip_number(D2_ch),
+        [.], ip_number(D3_ch),
+        [.], ip_number(D4_ch),
+        {
+                atom_chars(D1, D1_ch),
+                atom_chars(D2, D2_ch),
+                atom_chars(D3, D3_ch),
+                atom_chars(D4, D4_ch),
+                atomic_list_concat([D1, '.', D2, '.', D3, '.', D4], Ip) 
+        }.
 
 ip_number(Dig) --> [D1, D2, D3], 
         { valid_ip_number(D1, D2, D3), Dig = [D1, D2, D3] }.
 
 % PORT %
-port(Port) --> [:], port_valid(Port).
-port(['8', '0']) --> [].
+port(Port) --> [:], port_valid(Port_ch), { atom_chars(Port, Port_ch)}.
+port('80') --> [].
 
 port_valid(Port) --> [P_head], { char_type(P_head, digit) },
         port_valid(P_rest), { Port = [P_head | P_rest] }.
@@ -161,19 +172,22 @@ valid_ip_number(D1, D2, D3) :-
         Number >= 0,
         Number =< 255.
 
+handle_whitespace([], []).
+handle_whitespace([' ' | St], [['%', '2', '0'] | NSt]) :- handle_whitespace(St, NSt).
+handle_whitespace([S | St], [S | NSt]) :-
+        dif(S, ' '),
+        handle_whitespace(St, NSt).
+
 uri_parse(URIString, uri(Sch, UI, Ho, Po, Pa, Qu, Fr)) :-
-        phrase(uri(Sch, UI, Ho, Po, Pa, Qu, Fr), URIString).
+        handle_whitespace(URIString, WhiteURI),
+        flatten(WhiteURI, NormURI),
+        phrase(uri(Sch, UI, Ho, Po, Pa, Qu, Fr), NormURI).
 
 % TEST %
-test_uri_1_success :-
-        uri_parse("http://www.google.com/we/dir?query#fragment", _).
-test_uri_2_success :-
-        uri_parse("http://www.google.com/we/dir?query", _).
-test_uri_3_success :-
-        uri_parse("http://www.google.com/we/dir", _).
-test_uri_4_success :-
-        uri_parse("http://www.google.com/", _).
-test_uri_5_success :-
-        uri_parse("http://www.google.com/we/", _).
-test_uri_6_success :-
+test_success :-
+        uri_parse("http://www.google.com/we/dir?query#fragment", _),
+        uri_parse("http://www.google.com/we/dir?query", _),
+        uri_parse("http://www.google.com/we/dir", _),
+        uri_parse("http://www.google.com/", _),
+        uri_parse("http://www.google.com/we/", _),
         uri_parse("http:", _).
